68c1eb633a86991fad2e145b89806c26c07d90bd
hacknorris <hacknorris123456789@gmail.com>
2022-01-17 11:19:47 +0100

    main commit

---
+++ sus.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+luck=$(($RANDOM %5))
+case $luck in
+    1)
+        echo "----------------|"
+        echo " 69             |"
+        echo " ||     amogus  |"
+        echo "      ඞ ^       |"
+        echo "----------------|";;
+
+    2)
+        echo "   ___   "
+        echo "  /   \  "
+        echo "  |- -|  "
+        echo "  \_-_/  "
+        echo " B R U H ";;
+    3)
+	echo "ad| ඞ  ඞ  ඞ    x"
+	echo "--   ඞ   ඞ      "
+	echo "       ඞ        "
+	echo "----------------"
+	echo "play minecraft >";;
+    4)
+	echo "| h | | a | |p p| | y |"
+	echo "|v a| |len| |tin| |e s|"
+	echo "| d | | a | | y | |<3 |"
+	echo "  |     |     |     |"
+	echo "     P-P tiny :'D";;
+    5)
+	echo "★  ☆✦  ✸"
+	echo "★ ✦ ☆  ✸"
+	echo "☆ 🌍  ✦"
+	echo "☆ ✸ ★ "
+	echo "i got a pic"
+	echo "of your home and backyard"
+	echo "are ya scared?";;
+
+
+    *)
+	echo "|win xp  [x]|"
+	echo "|           |"
+	echo "|task failed|"
+	echo "|succesfully|"
+	echo "|  [OK]     |";;
+esac
